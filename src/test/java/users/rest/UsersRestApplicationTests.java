3c555fa337986f0ef9698c38b88173e0110a76a9
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-01-16 11:24:56 +0200

    Created new blank project

---
+++ src/test/java/users/rest/UsersRestApplicationTests.java
@@ -0,0 +1,13 @@
+package users.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
