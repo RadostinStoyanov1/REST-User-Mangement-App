5465f211608e93e4ee2dd41bae46791ae047f634
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-01-28 11:18:42 +0200

    Adding validation in UserEntity class as well

--- src/main/java/users/rest/model/entity/UserEntity.java
+++ src/main/java/users/rest/model/entity/UserEntity.java
@@ -1,6 +1,7 @@
 package users.rest.model.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
@@ -12,14 +13,22 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(name = "first_name", nullable = false)
+    @NotEmpty
+    @Size(min = 2, max = 30)
     private String firstName;
     @Column(name = "last_name", nullable = false)
+    @NotEmpty
+    @Size(min = 2, max = 30)
     private String lastName;
     @Column(name = "birth_date", nullable = false)
+    @Past
     private LocalDate birthDate;
     @Column(name = "phone_number", nullable = false, unique = true)
+    @Pattern(regexp = "[0-9]+")
+    @Size(min = 10, max = 10)
     private String phoneNumber;
     @Column(name = "email", nullable = false, unique = true)
+    @Email
     private String email;
 
     public Long getId() {
