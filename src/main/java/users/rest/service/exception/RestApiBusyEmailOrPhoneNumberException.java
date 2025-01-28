d75c581133479d82d14bd4aa6807fdf7ea0d5ed9
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-01-28 10:52:35 +0200

    Change http status of BusyEmailOrPhone Exception

--- src/main/java/users/rest/service/exception/RestApiBusyEmailOrPhoneNumberException.java
+++ src/main/java/users/rest/service/exception/RestApiBusyEmailOrPhoneNumberException.java
@@ -3,22 +3,22 @@ package users.rest.service.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.METHOD_NOT_ALLOWED)
+@ResponseStatus(code = HttpStatus.CONFLICT)
 public class RestApiBusyEmailOrPhoneNumberException extends RuntimeException {
-    private final Object email;
-    private final Object phone;
+    private final String email;
+    private final String phone;
 
-    public RestApiBusyEmailOrPhoneNumberException(String message, Object email, Object phone) {
+    public RestApiBusyEmailOrPhoneNumberException(String message, String email, String phone) {
         super(message);
         this.email = email;
         this.phone = phone;
     }
 
-    public Object getId() {
+    public String getId() {
         return email;
     }
 
-    public Object getPhone() {
+    public String getPhone() {
         return phone;
     }
 }
