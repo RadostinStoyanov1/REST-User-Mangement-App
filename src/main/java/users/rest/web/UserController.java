4f48a9c89bbcc3cd32b004c88e85d76a44a0dd52
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-01-31 21:14:43 +0200

    Adding of Swagger descriptions of methods in user Controller

--- src/main/java/users/rest/web/UserController.java
+++ src/main/java/users/rest/web/UserController.java
@@ -1,5 +1,10 @@
 package users.rest.web;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +20,10 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
+@Tag(
+        name = "Users",
+        description = "The controller responsible for users management"
+)
 public class UserController {
     private final UserService userService;
 
@@ -22,24 +31,81 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "201",
+                            description = "Created user",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = UserDTO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "If the user doesn't pass the validation of AddUserDTO"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "If the user has existing email or phone"
+                    )
+            }
+    )
     @PostMapping
     public ResponseEntity<UserDTO> createUser(@Valid @RequestBody AddUserDTO addUserDTO) {
         UserDTO userResponse = userService.createUserEntity(addUserDTO);
         return new ResponseEntity<UserDTO>(userResponse, HttpStatus.CREATED);
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Found user",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = UserDTO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "If user is not found"
+                    )
+            }
+    )
     @GetMapping("/{id}")
     public ResponseEntity<UserDTO> getByUuid(@PathVariable("id") UUID id) {
         return ResponseEntity
                 .ok(userService.getUserByUuid(id));
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "Got all users",
+            content = {
+                    @Content(mediaType = "application/json")
+            }
+    )
     @GetMapping("/all")
     public ResponseEntity<List<UserDTO>> getAllUsers(@RequestParam(name = "pattern", required = false) String pattern) {
         List<UserDTO> allFoundUsers = userService.getAllUsers(pattern);
         return ResponseEntity.ok(allFoundUsers);
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "Check if user with requested email exist",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BooleanResultDTO.class)
+                    )
+            }
+    )
     @GetMapping("/by-email")
     public ResponseEntity<BooleanResultDTO> checkIfUserWithPatternEmailExists(@RequestParam("pattern") String pattern) {
         return ResponseEntity.ok(
@@ -47,6 +113,16 @@ public class UserController {
         );
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "Check if user with requested phone number exist",
+            content = {
+                    @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = BooleanResultDTO.class)
+                    )
+            }
+    )
     @GetMapping("/by-phone")
     public ResponseEntity<BooleanResultDTO> checkIfUserWithPatternPhoneExists(@RequestParam("pattern") String pattern) {
         return ResponseEntity.ok(
@@ -54,12 +130,56 @@ public class UserController {
         );
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Updated user",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = UserDTO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "If the user doesn't pass the validation of UpdateUserDTO"
+                    ),
+                    @ApiResponse(
+                            responseCode = "500",
+                            description = "If the user has existing email or phone by another user"
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "If user with requested Uuid does not exist"
+                    )
+            }
+    )
     @PutMapping
     public ResponseEntity<UserDTO> updateUser(@Valid @RequestBody UpdateUserDTO updateUserDTO) {
         UserDTO userResponse = userService.updateUserByUuid(updateUserDTO);
         return new ResponseEntity<UserDTO>(userResponse, HttpStatus.OK);
     }
 
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Deleted user",
+                            content = {
+                                    @Content(
+                                            mediaType = "application/json",
+                                            schema = @Schema(implementation = BooleanResultDTO.class)
+                                    )
+                            }
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "If user with requested uuid is not found"
+                    )
+            }
+    )
     @DeleteMapping("/{id}")
     public ResponseEntity<BooleanResultDTO> deleteUserById(@PathVariable("id") UUID uuid) {
         userService.deleteUser(uuid);
